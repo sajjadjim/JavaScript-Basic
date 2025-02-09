bfdefbd988bb64b5de7bd898674f0e6ab9291005
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-09 15:48:06 +0600

    @sajjadjim-cse
    new commit

---
+++ ES6/Async Await.js
@@ -0,0 +1,20 @@
+async function myFunction() {
+    return "Hello";
+  }
+async function getFile() {
+    let myPromise = new Promise(function(resolve) {
+      let req = new XMLHttpRequest();
+      req.open('GET', "mycar.html");
+      req.onload = function() {
+        if (req.status == 200) {
+          resolve(req.response);
+        } else {
+          resolve("File not Found");
+        }
+      };
+      req.send();
+    });
+    document.getElementById("demo").innerHTML = await myPromise;
+  }
+  
+  getFile();
