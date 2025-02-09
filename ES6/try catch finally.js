54fa475b8e460e06f5fb775f20c19bd530af1cce
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-09 15:17:26 +0600

    @sajjadjim-cse
    new finally and try catch code write

---
+++ ES6/try catch finally.js
@@ -0,0 +1,12 @@
+try{
+console.log("My name is JIM")
+//addEventListener,a
+}
+
+catch(jimError){
+    console.log("Find the problem code  !!!!")
+}
+
+finally{
+    console.log("Finally code the run terminal here !!!!")
+}
