8b8ed1dc123cfcf43bb8e193f6aae87785e13bc8
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-09 16:41:26 +0600

    Update callback and higherorder funcion.js
    call back
    
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- ES6/callback and higherorder funcion.js
+++ ES6/callback and higherorder funcion.js
@@ -6,6 +6,5 @@ let higherOrderfunction = ( name , age , callBackfunction) => {
     const details = `My name is ${name}, I am ${age}  old and`
     callBackfunction(details)
 }
-
 higherOrderfunction("JIM" , 25 , display)
 
