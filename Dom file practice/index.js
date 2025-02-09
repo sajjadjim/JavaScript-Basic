81b070c7646d65edfebbbcf0dc0adb4f3d5f7236
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-10 00:51:57 +0600

    Update index.js
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- Dom file practice/index.js
+++ Dom file practice/index.js
@@ -2,7 +2,6 @@
 // function clickme(){
 //     document.getElementById("id1").innerHTML = Date()
 // }
-
 let name1 = document.getElementsByClassName("name1")[0];
 name1.innerHTML = "My name is jim"
 
@@ -10,4 +9,4 @@ let name2 = document.getElementsByClassName("name2")[0];
 name2.innerHTML= "HTML part 2 class practice Here"
 
 let id1 = document.getElementById("id1");
-id1.innerHTML = "Id javascript file added practice"
+id1.innerHTML = "Id javascript file added practice \n new added file some"
