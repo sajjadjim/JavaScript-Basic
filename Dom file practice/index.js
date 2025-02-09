09c4856e6789b02b014172e5db2ceaff0c4bc676
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-09 23:40:04 +0600

    New Dom File added
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

---
+++ Dom file practice/index.js
@@ -0,0 +1,11 @@
+
+// function clickme(){
+//     document.getElementById("id1").innerHTML = Date()
+// }
+
+let name1 = document.getElementsByClassName("name1")[0];
+name1.innerHTML = "My name is jim"
+
+let name2 = document.getElementsByClassName("name2")[0];
+name2.innerHTML= "HTML part 2 class practice Here"
+
