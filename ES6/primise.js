d31281716953372ee06ab0de315c40d905056431
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-09 15:34:04 +0600

    @sajjadjim-cse
    new code added

---
+++ ES6/primise.js
@@ -0,0 +1,20 @@
+let userInput = true;
+//let userInput = false;
+let promise = new Promise((solve,rejected)=>{
+    if(userInput){
+        solve([{username:"sajjad_jim" ,email:"sajjadjim15@gmail.com"},
+            {username:"Labonno", email:"salma@gmail.com"}
+        ])
+    }
+    else{
+        rejected("User access is deny !!!");
+    }
+})
+
+promise.then((solve_)=>{
+    console.log(solve_)
+})
+
+promise.catch((error)=>{
+console.log(error)
+})
