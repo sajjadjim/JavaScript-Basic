a16e7ae45e2412e05d01c01054301452762ba07f
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-10 17:19:37 +0600

    new
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

---
+++ Dom file practice--/index.js
@@ -0,0 +1,10 @@
+let name_1 = document.getElementsByClassName("name1")[0];
+name_1.innerHTML = "My name is Sajjad Hosain JIM";
+
+
+let id1 = document.getElementById("id1");
+id1.innerHTML = "H2 tag show on the file....."
+
+function btn(){
+    document.getElementById("show").innerHTML = Date()
+}
