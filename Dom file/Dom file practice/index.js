2e87ea5a563d43a72925dc72c19616b6bf5ddd12
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2025-02-16 23:30:57 +0600

    Update index.js

--- Dom file/Dom file practice/index.js
+++ Dom file/Dom file practice/index.js
@@ -6,7 +6,7 @@ let name1 = document.getElementsByClassName("name1")[0];
 name1.innerHTML = "My name is jim"
 
 let name2 = document.getElementsByClassName("name2")[0];
-name2.innerHTML= "HTML part 2 class practice Here"
+name2.innerHTML= "HTML part 2 class practice Here...." 
 
 let id1 = document.getElementById("id1");
 id1.innerHTML = "Id javascript file added practice \n new added file some"
