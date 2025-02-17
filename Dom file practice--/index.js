84e0f32bc9a11c969a255cfefc5b471610295911
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-17 13:33:26 +0600

    Update index.js
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- Dom file practice--/index.js
+++ Dom file practice--/index.js
@@ -3,7 +3,7 @@ name_1.innerHTML = "My name is Sajjad Hosain  JIM";
 
 
 let id1 = document.getElementById("id1");
-id1.innerHTML = "H2 tag show on the file......."
+id1.innerHTML = "H2 tag show on the file........"
 
 function btn(){
     document.getElementById("show").innerHTML = Date()
