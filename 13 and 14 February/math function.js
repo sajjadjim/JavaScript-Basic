f80125d36de2dba4a522a8ab7bba14728dba1d65
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-17 21:12:40 +0600

    Update math function.js
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- 13 and 14 February/math function.js
+++ 13 and 14 February/math function.js
@@ -2,6 +2,9 @@ console.log(Math.PI);
 console.log("My name is Jim");
 
 console.log("max number find ->>"+ Math.max(1,2,45,67,45,66))
-console.log("minimun number -->" + Math.min(2,67,34,56,89,2,34,36))
+console.log("minimum number -->" + Math.min(2,67,34,56,89,2,34,36))
 console.log(Math.round(4.9))
 console.log(Math.abs(5 -16))
+
+//Math abs function some code write here
+console.log(Math.abs(-5 -6))
