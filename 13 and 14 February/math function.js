6a19613e3defa5a06e118c3fed4a46e5393e6c1c
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-18 17:39:25 +0600

    Update math function.js
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- 13 and 14 February/math function.js
+++ 13 and 14 February/math function.js
@@ -1,7 +1,7 @@
 console.log(Math.PI);
 console.log("My name is Jim");
 
-console.log("max number find ->>"+ Math.max(1,2,45,67,45,66,99))
+console.log("max number find ->>"+ Math.max(1,2,45,67,45,66,99,100))
 console.log("minimum number -->" + Math.min(2,67,34,56,89,2,34,36))
 console.log(Math.round(4.9))
 console.log(Math.abs(5 -16))
