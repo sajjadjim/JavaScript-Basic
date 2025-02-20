35be237592b2ad7b0e26ad46f4b619be5b5a2a06
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-20 19:36:36 +0600

    Create array.js

---
+++ array.js
@@ -0,0 +1,17 @@
+// const array ={
+//     '0' : "JIM",
+//     '1' : "Labanna",
+//     '2' : "TOM",
+//     '3' : "Jerry",
+// }
+// console.log(array[0]);
+
+
+function array (num1 , num2 ){
+const sum = num1 + num2 ;
+// return sum;
+// console.log(arguments);
+console.log(arguments[3]);
+}
+array(5,6,11,22,34,66);
+console.log(array(5,6));
