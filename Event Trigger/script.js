42009f1edb5995f5d5ce3b1be4744451f973ec77
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-02-22 19:56:20 +0600

    Event work
    Co-Authored-By: sajjadjim-cse <197888910+sajjadjim-cse@users.noreply.github.com>

--- Event Trigger/script.js
+++ Event Trigger/script.js
@@ -1,12 +1,18 @@
-
+// type 1
 function makeRed(){
     document.body.style.backgroundColor = 'red'
 }
-
+// create function and Call function on the HTML file 
 function makeBlue(){
     document.body.style.backgroundColor = 'lightblue'
 }
 
+
+
+
+// Type 2
+// create function here and also call the html By using ID Tag call 
+// yellow Button function 
 const yellowBTN = document.getElementById('yellow-btn');
 // style  given the button 
 yellowBTN.style.border ='2px solid black'
@@ -16,4 +22,14 @@ yellowBTN.style.cursor = 'pointer'
 // create a function for Onclick 
 yellowBTN.onclick = function makeYellow(){
 document.body.style.backgroundColor = 'yellow'
+}
+
+// reset Button 
+const resetColor = document.getElementById('reset-color');
+resetColor.style.border ='2px solid black'
+resetColor.style.borderRadius = '5px'
+resetColor.style.padding = '5px'
+resetColor.style.cursor = 'pointer'
+resetColor.onclick= function resetCOLOR(){
+document.body.style.backgroundColor = 'white'
 }
