f4c5a9c1189430f46dcfbc868504ae9ec8dbe62d
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-03-05 01:09:50 +0600

    Update spread.js
    Co-Authored-By: Mahfujur Rahman <141043578+mahfujur221@users.noreply.github.com>

--- ES6 Programming Hero/spread.js
+++ ES6 Programming Hero/spread.js
@@ -6,3 +6,8 @@ const newArray = ['Siam' ,...array]
 
 console.log(newArray)
 
+
+// Math File find the largest Number using javacsript
+let number = [1,2,34,5,6,78,99,33,45,56]
+
+console.log("The largest Number -> ",Math.max(...number));
